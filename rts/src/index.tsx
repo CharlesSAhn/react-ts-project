b42ce6665a5d5f25121fdbdc7addb2502df97a9d
Charles Ahn <charlesahn5@gmail.com>
2023-06-12 09:43:12 -0400

    inital commit

---
+++ rts/src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import GuestList from "./state/GustList";
+import UserSearch from "./refs/UserSearch";
+import EventComponent from "./events/EventComponent";
+
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+    return <div>
+        <UserSearch />
+    </div>
+};
+
+root.render(<App />);
---
+++ rts/src/props/Parent.tsx
@@ -0,0 +1,8 @@
+import { Child, ChildAsFC } from './Child'
+
+const Parent = () => {
+    return <ChildAsFC color="red" onClick={() => console.log("click")} >
+        </ChildAsFC>
+};
+
+export default Parent;
